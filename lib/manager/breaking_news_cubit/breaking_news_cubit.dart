2fe7887c946b9031a1fcc3314af38ce1ced265b0
Ahmed Maher <zamalek.545r5@gmail.com>
2023-04-12 09:58:44 +0200

    linking cubits to news repo

--- lib/manager/breaking_news_cubit/breaking_news_cubit.dart
+++ lib/manager/breaking_news_cubit/breaking_news_cubit.dart
@@ -1,8 +1,18 @@
+import 'dart:ffi';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:news_app/repo/news_repo.dart';
 
 part 'breaking_news_state.dart';
 
 class BreakingNewsCubit extends Cubit<BreakingNewsState> {
-  BreakingNewsCubit() : super(BreakingNewsInitial());
+  BreakingNewsCubit(this.newsRepo) : super(BreakingNewsInitial());
+
+  final NewsRepo newsRepo;
+
+  Future<void> getBreakingNews() async {
+    emit(BreakingNewsLoading());
+    await newsRepo.getBreakingNews();
+  }
 }
--- lib/manager/recommendation_news_cubit/recommendation_news_cubit.dart
+++ lib/manager/recommendation_news_cubit/recommendation_news_cubit.dart
@@ -1,8 +1,17 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../repo/news_repo.dart';
+
 part 'recommendation_news_state.dart';
 
 class RecommendationNewsCubit extends Cubit<RecommendationNewsState> {
-  RecommendationNewsCubit() : super(RecommendationNewsInitial());
+  RecommendationNewsCubit(this.newsRepo) : super(RecommendationNewsInitial());
+
+  final NewsRepo newsRepo;
+
+  Future<void> getRecommendationNews() async {
+    emit(BreakingNewsLoading());
+    await newsRepo.getRecommendationNews();
+  }
 }
